88414821f93c6677fb1eea2693424a9e3012f446
David Kajpust <kajpustd@gmail.com>
2020-01-06 15:51:26 -0500

    Stub out handlers

---
+++ subgraph/src/mappings/ethereumDIDRegistry.ts
@@ -0,0 +1,19 @@
+// import { BigInt, BigDecimal, Address, log } from '@graphprotocol/graph-ts'
+
+import {
+  DIDOwnerChanged,
+  DIDDelegateChanged,
+  DIDAttributeChanged,
+} from '../types/EthereumDIDRegistry/EthereumDIDRegistry'
+
+export function handleDIDOwnerChanged(event: DIDOwnerChanged): void {
+  // TODO: implement this
+}
+
+export function handleDIDDelegateChanged(event: DIDDelegateChanged): void {
+  // TODO: implement this
+}
+
+export function handleDIDAttributeChanged(event: DIDAttributeChanged): void {
+  // TODO: implement this
+}
--- subgraph/src/mappings/everest.ts
+++ subgraph/src/mappings/everest.ts
@@ -1,5 +1,64 @@
-import { ApplicationMade } from '../../generated/Everest/Everest'
+// import { BigInt, BigDecimal, Address, log } from '@graphprotocol/graph-ts'
 
-export function handleProjectAdded(event: handleApplicationMade): void {
+import {
+  ApplicationMade,
+  MemberExited,
+  MemberOwnerChanged,
+  DelegateAdded,
+  DelegateRevoked,
+  MemberOffChainDataEdited,
+  CharterUpdated,
+  Withdrawal,
+  MemberChallenged,
+  SubmitVote,
+  ChallengeFailed,
+  ChallengeSucceeded,
+} from '../types/Everest/Everest'
+
+export function handleApplicationMade(event: ApplicationMade): void {
+  // TODO: implement this
+}
+
+export function handleMemberExited(event: MemberExited): void {
+  // TODO: implement this
+}
+
+export function handleMemberOwnerChanged(event: MemberOwnerChanged): void {
+  // TODO: implement this
+}
+
+export function handleDelegateAdded(event: DelegateAdded): void {
+  // TODO: implement this
+}
+
+export function handleDelegateRevoked(event: DelegateRevoked): void {
+  // TODO: implement this
+}
+
+export function handleMemberOffChainDataEdited(event: MemberOffChainDataEdited): void {
+  // TODO: implement this
+}
+
+export function handleCharterUpdated(event: CharterUpdated): void {
+  // TODO: implement this
+}
+
+export function handleWithdrawal(event: Withdrawal): void {
+  // TODO: implement this
+}
+
+export function handleMemberChallenged(event: MemberChallenged): void {
+  // TODO: implement this
+}
+
+export function handleSubmitVote(event: SubmitVote): void {
+  // TODO: implement this
+}
+
+export function handleChallengeFailed(event: ChallengeFailed): void {
+  // TODO: implement this
+}
+
+export function handleChallengeSucceeded(event: ChallengeSucceeded): void {
   // TODO: implement this
 }
