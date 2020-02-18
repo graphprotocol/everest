e87cde6e69ed61f2b952087d0aa079ad51e0acbe
Nestor <nestor09amesty@gmail.com>
2020-02-19 00:28:59 +0100

    Added missing interfaces

--- subgraph/mutations/src/types.ts
+++ subgraph/mutations/src/types.ts
@@ -23,9 +23,74 @@ export interface addProjectArgs{
   github: string
   twitter: string
   isRepresentative: boolean
-  categories: Array<any>
+  categories: Array<Category>
 }
 
 export interface removeProjectArgs{
   projectId: string
+}
+
+interface Category {
+  id: string
+  description: string
+  slug: string
+  projects: Project[]
+  subcategories: Category[]
+  parentCategory: Category
+}
+
+interface Project {
+  id: string
+  name: string
+  description: string
+  website: string
+  twitter: string
+  github: string
+  avatar: string
+  image: string
+  categories: Category[]
+  isRepresentative: boolean
+  owner: User
+  currentChallenge: Challenge
+  pastChallenges: Challenge[]
+  membershipStartTime: number
+  delegates: string[]
+  delegateValidities: number[]
+  totalVotes: number
+  votes: Vote[]
+}
+
+interface Challenge {
+  id: string
+  description: string
+  endTime: number
+  votesFor: number
+  votesAgainst: number
+  project: Project
+  owner: string
+  votes: Vote[]
+  resolved: boolean
+}
+
+interface User {
+  id: string
+  name: string
+  bio: string
+  projects: Project[]
+  challenges: Challenge[]
+  votes: Project[]
+}
+
+interface Vote {
+  id: string
+  voter: Project
+  challenge: Challenge
+  choice: Choice
+  weight: number
+}
+
+enum Choice {
+  Null,
+  Yes,
+  No
 }
