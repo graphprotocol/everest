7ef1c87d1561f9315c9330810ffecd0ef1ee6f41
Nestor <nestor09amesty@gmail.com>
2020-02-18 13:06:00 +0100

    Apollo client supporting mutations (URIs missing))

--- ui/src/utils/apollo/client.js
+++ ui/src/utils/apollo/client.js
@@ -1,9 +1,56 @@
-import ApolloClient from 'apollo-boost'
-import fetch from 'isomorphic-fetch'
+import { InMemoryCache } from 'apollo-boost'
+import ApolloClient from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { getMainDefinition } from 'apollo-utilities'
+import { split } from 'apollo-link'
+import tokenRegistryMutations from 'token-registry-mutations'
+import { createMutations, createMutationsLink } from '@graphprotocol/mutations'
+
+//TODO: URIs?
+const networkURI = process.env.GATSBY_NETWORK_URI
+const ipfsURI = process.env.GATSBY_IPFS_HTTP_URI
+
+const mutations = createMutations({
+  mutations: {
+    resolvers: tokenRegistryMutations.resolvers,
+    config: tokenRegistryMutations.config,
+    stateBuilder: tokenRegistryMutations.stateBuilder,
+  },
+  subgraph: 'everest',
+  node: ipfsURI,
+  config: {
+    ethereum: async () => {
+      const { ethereum } = window
+
+      if (!ethereum) {
+        throw Error('Please install metamask')
+      }
+
+      await ethereum.enable()
+      return window.web3.currentProvider
+    },
+    ipfs: ipfsURI,
+  },
+})
+
+const queryLink = createHttpLink({
+  uri: `${networkURI}/subgraphs/name/graphprotocol/everest`,
+})
+
+const mutationLink = createMutationsLink({ mutations })
+
+const link = split(
+  ({ query }) => {
+    const node = getMainDefinition(query)
+    return node.kind === 'OperationDefinition' && node.operation === 'mutation'
+  },
+  mutationLink,
+  queryLink
+)
 
 const client = new ApolloClient({
-  uri: 'https://eu1.prisma.sh/nevena-djaja/ui-mocks/dev',
-  fetch: fetch
+  link,
+  cache: new InMemoryCache(),
 })
 
 export default client
