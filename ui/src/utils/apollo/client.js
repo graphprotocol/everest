0bdbad1ed75891d6f8f218015f683deb806f66c3
Nena <nevena.djaja@gmail.com>
2020-02-19 15:06:16 -0800

    Update Apollo client and packages

--- ui/src/utils/apollo/client.js
+++ ui/src/utils/apollo/client.js
@@ -1,9 +1,55 @@
-import ApolloClient from 'apollo-boost'
-import fetch from 'isomorphic-fetch'
+import { InMemoryCache } from 'apollo-boost'
+import ApolloClient from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { getMainDefinition } from 'apollo-utilities'
+import { split } from 'apollo-link'
+import everestMutations from 'everest-mutations'
+import { createMutations, createMutationsLink } from '@graphprotocol/mutations'
+
+const networkURI = process.env.GATSBY_NETWORK_URI
+const ipfsURI = process.env.GATSBY_IPFS_HTTP_URI
+
+const mutations = createMutations({
+  mutations: {
+    resolvers: everestMutations.resolvers,
+    config: everestMutations.config,
+    stateBuilder: everestMutations.stateBuilder,
+  },
+  subgraph: 'everest',
+  node: ipfsURI,
+  config: {
+    ethereum: async () => {
+      const { ethereum } = window
+
+      if (!ethereum) {
+        throw Error('Please install metamask')
+      }
+
+      await ethereum.enable()
+      return window.web3.currentProvider
+    },
+    ipfs: ipfsURI,
+  },
+})
+
+const queryLink = createHttpLink({
+  uri: `${networkURI}/subgraphs/name/graphprotocol/everest`,
+})
+
+const mutationLink = createMutationsLink({ mutations })
+
+const link = split(
+  ({ query }) => {
+    const node = getMainDefinition(query)
+    return node.kind === 'OperationDefinition' && node.operation === 'mutation'
+  },
+  mutationLink,
+  queryLink,
+)
 
 const client = new ApolloClient({
-  uri: 'https://api.staging.thegraph.com/subgraphs/name/graphprotocol/everest',
-  fetch: fetch,
+  link,
+  cache: new InMemoryCache(),
 })
 
 export default client
