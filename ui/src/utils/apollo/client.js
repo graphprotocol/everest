ced046316092c18929694eb374008d4a27863df3
Nestor <nestor09amesty@gmail.com>
2020-02-19 01:38:43 +0100

    Commented mutations link

--- ui/src/utils/apollo/client.js
+++ ui/src/utils/apollo/client.js
@@ -3,46 +3,46 @@ import ApolloClient from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { getMainDefinition } from 'apollo-utilities'
 import { split } from 'apollo-link'
-import tokenRegistryMutations from 'token-registry-mutations'
-import { createMutations, createMutationsLink } from '@graphprotocol/mutations'
+//import tokenRegistryMutations from 'token-registry-mutations'
+//import { createMutations, createMutationsLink } from '@graphprotocol/mutations'
 
 const networkURI = process.env.GATSBY_NETWORK_URI
-const ipfsURI = process.env.GATSBY_IPFS_HTTP_URI
+// const ipfsURI = process.env.GATSBY_IPFS_HTTP_URI
 
-const mutations = createMutations({
-  mutations: {
-    resolvers: tokenRegistryMutations.resolvers,
-    config: tokenRegistryMutations.config,
-    stateBuilder: tokenRegistryMutations.stateBuilder,
-  },
-  subgraph: 'everest',
-  node: ipfsURI,
-  config: {
-    ethereum: async () => {
-      //TODO: support multiple wallets
-      return window.web3.currentProvider
-    },
-    ipfs: ipfsURI,
-  },
-})
+// const mutations = createMutations({
+//   mutations: {
+//     resolvers: tokenRegistryMutations.resolvers,
+//     config: tokenRegistryMutations.config,
+//     stateBuilder: tokenRegistryMutations.stateBuilder,
+//   },
+//   subgraph: 'everest',
+//   node: ipfsURI,
+//   config: {
+//     ethereum: async () => {
+//       //TODO: support multiple wallets
+//       return window.web3.currentProvider
+//     },
+//     ipfs: ipfsURI,
+//   },
+// })
 
 const queryLink = createHttpLink({
-  uri: `${networkURI}/subgraphs/name/graphprotocol/everest`,
+  uri: `${networkURI}/subgraph/graphprotocol/everest`,
 })
 
-const mutationLink = createMutationsLink({ mutations })
+// const mutationLink = createMutationsLink({ mutations })
 
-const link = split(
-  ({ query }) => {
-    const node = getMainDefinition(query)
-    return node.kind === 'OperationDefinition' && node.operation === 'mutation'
-  },
-  mutationLink,
-  queryLink
-)
+// const link = split(
+//   ({ query }) => {
+//     const node = getMainDefinition(query)
+//     return node.kind === 'OperationDefinition' && node.operation === 'mutation'
+//   },
+//   mutationLink,
+//   queryLink
+// )
 
 const client = new ApolloClient({
-  link,
+  link: queryLink,
   cache: new InMemoryCache(),
 })
 
