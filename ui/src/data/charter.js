833f6d24c8b2335ffd24781a76527ad09c0fa1f8
Nena <nevena.djaja@gmail.com>
2020-03-11 09:35:48 -0700

    Add charter page

---
+++ ui/src/data/charter.js
@@ -0,0 +1,131 @@
+import React from 'react'
+
+export default [
+  {
+    heading: 'Scope',
+    body: (
+      <p>
+        Everest is a shared registry of projects that is curated by registry
+        members.
+        <br />
+        <br />
+        Created to serve the Web3 ecosystem, Everest aims to provide
+        transparency about organizations and communities and to be the go-to
+        source for verifiable information. This will include project details,
+        links, job postings, events and more. As Everest grows, so will its
+        utility as other decentralized applications and on-chain data is
+        integrated with registry projects.
+        <br />
+        <br />
+        Projects to be listed on Everest include any organizations building on
+        decentralized technology, integrating cryptocurrencies or supporting the
+        growth of Web3. Everest will be composed of but is not limited to
+        decentralized applications, protocols, NGOs, research organizations,
+        venture and hedge funds. Web3 can include distributed systems, p2p
+        networking or blockchain technology. Projects can be mapped to
+        categories and sub-categories to allow for easy search and clear
+        categorization of projects across Web3 verticals.
+        <br />
+        <br />
+        The Everest Registry uses the ERC-1056 identity standard for projects to
+        enable composability of project identity across other decentralized
+        applications.
+      </p>
+    ),
+  },
+  {
+    heading: 'Project Listing Guidelines',
+    body: (
+      <p>
+        Everest's mission is to curate a shared registry of projects that are
+        operational and high-quality. The value of Everest to the broader
+        ecosystem depends on the integrity and accuracy of information in
+        project listings.
+        <br />
+        <br />
+        Everest is composed of the community's efforts to create a reliable
+        tracker of organizations working together.
+        <br />
+        <br />
+        Projects should have accurate details: name, description, project logo
+        and image and correct links to the website, Github and Twitter. Projects
+        should also be assigned categories and sub-categories based on the
+        verticals they belong to.
+        <br />
+        <br />
+        At all times, the Everest registry is being curated. If projects have
+        inaccurate information or are being fraudulently misrepresented, a
+        registry member can challenge the project requiring other members to
+        vote on the validity of the listing.
+      </p>
+    ),
+  },
+  {
+    heading: 'Everest Curation',
+    body: (
+      <p>
+        All users are incentivized to maintain the list's quality since there is
+        limited advantage to...
+        <br />
+        <br /> Everest is building toward a decentralized future where no
+        privileged group has control over public data. The Everest registry aims
+        to be a public dataset about projects, that is maintained by diverse
+        stakeholders who agree on the contents of the shared registry with
+        neutrality.
+        <br />
+        <br />
+        Anyone can add a project to Everest by paying a 10 DAI listing fee to
+        the Everest Reserve Bank. The listing fee helps ensure skin in the game
+        for maintaining the registry's quality. If a project representative (eg.
+        an employee, contractor, investor) adds the project, makes sure to claim
+        the project so others know that the listing will be maintained by the
+        native project.
+        <br />
+        <br />
+        Once a project is added, the project owner can help maintain Everest's
+        quality by challenging existing projects on the registry or voting on
+        challenges, on behalf of the project. Registry listings should only be
+        challenged if there is reasonable belief that the project does not
+        belong on the Everest registry:
+        <br />
+        <br />
+        a) the project details are inaccurate (eg. outdated information, broken
+        links)
+        <br /> b) the project's categories or sub-categories are inaccurate
+        <br /> c) the project is being fraudulently misrepresented
+        <br />
+        <br />
+        Anyone can challenge a listing by staking 10 DAI. If the challenge is
+        successful, the stake is returned and the challenger is awarded 1 DAI.
+        If the challenge is unsuccessful, the challenger loses their stake and
+        it's added to the Everest Reserve Bank. Once a challenge is complete,
+        any user can also resolve a challenge and earn 1 DAI for resolving
+        complete challenges.
+        <br />
+        <br /> Owners can vote on behalf of multiple projects or they can
+        delegate their voting rights. Each project's vote is weighted by time,
+        derived from how long the project has been a part of the Everest
+        registry.
+      </p>
+    ),
+  },
+  {
+    heading: 'Community Maintenance',
+    body: (
+      <p>
+        Everest was developed by The Graph and MetaCartel and will be maintained
+        by the community. The goal of Everest is to integrate project identities
+        with other useful applications and protocols. As a result, the Everest
+        Reserve Bank and community grants will be used to fund bounties to build
+        greater functionality into the Everest registry.
+        <br />
+        <br />
+        Check out the{' '}
+        <a href="https://github.com/graphprotocol/everest" target="_blank">
+          Everest Monorepo
+        </a>{' '}
+        to learn more and contribute.
+      </p>
+    ),
+  },
+]
---
+++ ui/src/pages/charter.js
@@ -0,0 +1,30 @@
+/** @jsx jsx */
+import { Styled, jsx, Box } from 'theme-ui'
+import charter from '../data/charter'
+
+const Charter = () => {
+  return (
+    <Box>
+      <Styled.h1 sx={{ textAlign: 'center', mb: 4 }}>Everest Charter</Styled.h1>
+      {charter.map(entry => (
+        <Box
+          sx={{
+            margin: '0 auto',
+            py: 6,
+            maxWidth: '640px',
+            width: '100%',
+            '& a': {
+              textDecoration: 'none',
+              color: 'secondary',
+            },
+          }}
+        >
+          <Styled.h3 sx={{ my: 4 }}>{entry.heading}</Styled.h3>
+          {entry.body}
+        </Box>
+      ))}
+    </Box>
+  )
+}
+
+export default Charter
